f779b242832767667cbf47bdb921d0c741ce0d07
Frederic Mes <fred@callitrix.com>
2019-04-23 21:04:58 +0200

    [LICENSE] - added copyright & license to main.c

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+/**
+ * Copyright (c) 2019 - Frederic Mes, RTLOC
+ * 
+ * This file is part of Zephyr-DWM1001.
+ *
+ *   Zephyr-DWM1001 is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Zephyr-DWM1001 is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Zephyr-DWM1001.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
+
 extern int dw_main(void);
 
 int main(void)
@@ -7,3 +27,5 @@ int main(void)
   while(1)
   {}
 }
+
+/* EOF */
